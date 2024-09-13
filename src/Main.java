3d954173f964595f4f76391955f9d4bdf5b5afc1
Arnold Shakirov <124413653+arnold-shakirov@users.noreply.github.com>
2024-09-13 11:13:32 -0400

    Added needed folders and code files

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static int postfixEvaluate(String expression) {
+        Stack<Integer> s = new Stack<Integer>();
+        Scanner input = new Scanner(expression);
+        while (input.hasNext()) {
+            if (input.hasNextInt()) { // an operand (integer)
+                s.push(input.nextInt());
+            } else { // an operator
+                String operator = input.next();
+                int operand2 = s.pop();
+                int operand1 = s.pop();
+                if (operator.equals("+")) {
+                    s.push(operand1 + operand2);
+                } else if (operator.equals("-")) {
+                    s.push(operand1 - operand2);
+                } else if (operator.equals("*")) {
+                    s.push(operand1 * operand2);
+                } else {
+                    s.push(operand1 / operand2);
+                }
+            }
+        }
+        return s.pop();
+    }
+
+    public static int PostPrefix(String expression) {
+        Scanner input = new Scanner(expression);
+        Stack<Integer> s = new Stack<Integer>();
+        while (input.hasNext()) {
+            if (input.hasNextInt()) {
+                s.push(input.nextInt());
+            } else {
+                String operator = input.next();
+                int operand2 = s.pop();
+                int operand1 = s.pop();
+                if (operator.equals("+")) {
+                    s.push(operand1 + operand2);
+                } else if (operator.equals("-")) {
+                    s.push(operand1 - operand2);
+                } else if (operator.equals("*")) {
+                    s.push(operand1 * operand2);
+                } else if (operator.equals("/")) {
+                    s.push(operand1 / operand2);
+                }
+            }
+        }
+        return s.pop();
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int end = postfixEvaluate(s);
+        }
+
+    }
